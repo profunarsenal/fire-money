aa7ae474a449b3c77e6c67ce2b2ea8d5e67be2ea
Dmitry Shiryaev <profunarsenal@gmail.com>
2022-06-04 16:51:09 +0400

    add title component

---
+++ src/components/SuggestionsSection.vue
@@ -0,0 +1,80 @@
+<template>
+  <section class="suggestions">
+    <div class="container">
+      <tittle-page class="suggestions-title" title="Лучшие предложения" />
+      <div class="suggestions-items">
+        <div class="suggestions-item">
+          <span class="suggestions-sticker">lite</span>
+          <h5 class="suggestions-item-title">
+            <span>15 000 ₽</span>
+            на
+            <span>7</span>
+            дней
+          </h5>
+          <div class="suggestions-item-date">
+            Дата возврата:
+            <span>12 января 2022</span>
+          </div>
+          <div class="suggestions-item-sum">
+            К возврату:
+            <span>45 500 ₽</span>
+          </div>
+        </div>
+        <div class="suggestions-item">
+          <span class="suggestions-sticker">lite</span>
+          <h5 class="suggestions-item-title">
+            <span>15 000 ₽</span>
+            на
+            <span>7</span>
+            дней
+          </h5>
+          <div class="suggestions-item-date">
+            Дата возврата:
+            <span>12 января 2022</span>
+          </div>
+          <div class="suggestions-item-sum">
+            К возврату:
+            <span>45 500 ₽</span>
+          </div>
+        </div>
+        <div class="suggestions-item">
+          <span class="suggestions-sticker">lite</span>
+          <h5 class="suggestions-item-title">
+            <span>15 000 ₽</span>
+            на
+            <span>7</span>
+            дней
+          </h5>
+          <div class="suggestions-item-date">
+            Дата возврата:
+            <span>12 января 2022</span>
+          </div>
+          <div class="suggestions-item-sum">
+            К возврату:
+            <span>45 500 ₽</span>
+          </div>
+        </div>
+      </div>
+      <p class="suggestions-mess">
+        * Расчёт не является публичное офертой и может отличаться от реальной
+        ставки кредитора
+      </p>
+    </div>
+  </section>
+</template>
+
+<script>
+import TittlePage from "@/components/TittlePage.vue";
+
+export default {
+  components: {
+    TittlePage,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.suggestions {
+  padding: 50px 0;
+}
+</style>
---
+++ src/components/TittlePage.vue
@@ -0,0 +1,24 @@
+<template>
+  <h2 class="title">{{ title }}</h2>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.title {
+  font-weight: 600;
+  font-size: 38px;
+  line-height: 45px;
+  color: #45515f;
+  text-align: center;
+}
+</style>
