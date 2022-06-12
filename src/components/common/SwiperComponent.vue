38b0d241637b673138e975813df1d22c1dc0ddfc
Dmitry Shiryaev <profunarsenal@gmail.com>
2022-06-12 15:46:42 +0400

    load swiper

---
+++ src/components/common/SwiperComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <swiper :modules="modules" :pagination="{ clickable: true }">
+    <swiper-slide></swiper-slide>
+  </swiper>
+</template>
+
+<script>
+import SwiperClass, { Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "vue-awesome-swiper";
+
+// import swiper module styles
+import "swiper/css";
+import "swiper/css/pagination";
+// more module style...
+
+export default {
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  setup() {
+    return {
+      modules: [Pagination],
+    };
+  },
+};
+</script>
